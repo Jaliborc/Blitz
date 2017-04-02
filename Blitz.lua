ccace21897947ca24fcfe48b24fa83c276521408
Joao Cardoso <contact@jaliborc.com>
2017-04-02 22:17:14 +0100

    Version 6.2

--- Blitz.lua
+++ Blitz.lua
@@ -1,5 +1,5 @@
 --[[
-Copyright 2009-2016 João Cardoso
+Copyright 2009-2017 João Cardoso
 Blitz is distributed under the terms of the GNU General Public License (or the Lesser GPL).
 This file is part of Blitz.
 
